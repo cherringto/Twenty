2e46849de9e6b83419c2f114afef186afb7a298a
Herrington <cherringto@FVFY37ADJ1WK.tld>
2022-03-30 12:05:01 -0500

    week8

--- app.js
+++ app.js
@@ -1,129 +1,27 @@
-alert("Welcome to 20! You and the computer will take turns drawing numbers. The goal is to get as close to 20 as you can without going over. The computer will stop at 16. Press ok to play!");
+function comparison(c, compWins, userWins, u) {
 
-var compWins = 0;
+    var userWins = 0
+    var compWins = 0
 
-var userWins = 0;
-
-do {
-
-    var compTotal = 0;
-
-    var userTotal = 0;
-
-
-    do {
-    
-        if (compTotal < 16) { 
-        
-        alert("It is the computer's turn.");
-        
-        var compNum = Math.floor(Math.random() * 10) + 1;
-        
-        alert(`The computer drew ${compNum}.`);
-        
-        compTotal = compTotal + compNum;
-    
-        alert(`The computer's total is ${compTotal}`)
-        
-        } else if (compTotal > 20) {
-
-            alert("You win! The computer went over 20.")
-            break;
-
-        } else if (compTotal >= 16) {
-        
-            alert(`The computer will stop at ${compTotal}.`)
-            
-        }
-    
-    
-    
-        if (userTotal > 20) {
-    
-        alert("Game over. You've gone over 20.")
-        break;
-        
-        } else if (userTotal <= 20) {
-    
-    
-        alert("Now it is your turn.");
-        
-        var draw = prompt("Do you want to draw? Type 'y' for yes and 'n' for no.");
-        
-        if (userTotal > 20) {
-        
-            alert("Game over. You've gone over 20.")
-            
-        
-        } else if (draw == "y") {
-        
-            var userNum = Math.floor(Math.random() * 10) + 1;
-            
-            alert(`You drew ${userNum}.`);
-            
-            userTotal = userTotal + userNum;
-        
-            alert(`Your total is ${userTotal}`) 
-            
-        } else if (draw == "n") {
-        
-            alert(`You will stop at ${userTotal}.`)
-            break;
-        
-        }
-    
-        }
-        
-    } while (draw == "y");
-
-    do {
-
-        if (compTotal <16) {
-
-            alert("It is the computer's turn.");
-        
-            var compNum = Math.floor(Math.random() * 10) + 1;
-        
-            alert(`The computer drew ${compNum}.`);
-        
-            compTotal = compTotal + compNum;
-    
-            alert(`The computer's total is ${compTotal}`)
-        
-        } else if (compTotal > 20) {
-
-            alert("You win! The computer went over 20!")
-            break;
-
-        } else if (compTotal >= 16) {
-        
-            alert(`The computer will stop at ${compTotal}.`)
-            
-        }
-
-    } while (compTotal < 16)
-
-
-
-    if (compTotal > 20) {
+    if (c > 20) {
 
         userWins++;
         alert("You win! The computer went over 20!")
         alert(`You've won ${userWins} times and the computer has won ${compWins} times.`);
 
-    } else if (userTotal > 20) {
+    } else if (u > 20) {
 
         compWins++;
         ("You lost. You went over 20.")
         alert(`You've won ${userWins} times and the computer has won ${compWins} times.`);
 
-    } else if (compTotal > userTotal) {
+    } else if (c > u) {
 
         compWins++
         alert("You lost. The computer was closer to 20 than you.")
         alert(`You've won ${userWins} times and the computer has won ${compWins} times.`)
 
-    } else if (userTotal > compTotal) {
+    } else if (u > c) {
 
         userWins++
         alert("You won! You were closer to 20 than the computer!")
@@ -131,17 +29,93 @@ do {
 
     }
 
+    
+    
+}
+
+function nameAsk() {
+    var askName = prompt(`What is your name?`)
+    alert(`Welcome to 20 ${askName}! You and the computer will take turns drawing numbers. The goal is to get as close to 20 as you can without going over. The computer will stop at 16. Press ok to play!`)
+    return askName
+}
+
+nameAsk()
+
+function ran() {
+    var rn = Math.floor(Math.random() * 10) + 1
+    return rn
+}
+
+function userFunction() { 
+    
+    var userRan;
+    var userTotal = 0
+     
+
+    do {
+    
+            
+        userRan = ran()
+                 
+        userTotal = userTotal + userRan;
+
+        if (userTotal >= 20) {
+            alert(userTotal)
+            break
+        }
+
+        var draw = prompt(`You drew ${userRan}. Your total is ${userTotal}. Would you like another number? Press n to stop`)
+      
+
+    } while (draw != "n")
+    
+    return userTotal
+}
+
+var u = userFunction()
+
+comparison()
+
+function compFunction() { 
+    
+    var compRan;
+    var compTotal = 0
+     
+
+    do {
+    
+            
+        compRan = ran()
+                 
+        compTotal = compTotal + compRan;
+
+        if (compTotal >= 16) {
+            break
+        }
+
+        alert(`The computer drew ${compRan}. Its total is ${compTotal}.`)
+      
+
+    } while (compTotal < 16)
+    
+    return compTotal
+          
+}
+
+var c = compFunction()
+
+comparison()
 
 
+
+function restart() {
+    
     var restart = prompt("Would you like to play again? Type 'y' for yes and 'n' for no.")
+    if (restart == "y") {
+        game()
+    }
+    return restart
 
-} while (restart == "y");
+}
 
-
-/*
-
-do while to get comp number -- if comp goes over 20, stop
-do while to get user number -- if user goes over 20, stop
-compare -- if user and comp under 20, then check greater than //  if user over 20, comp wins // if comp over 20, user wins
-
-github test comment
+restart()
